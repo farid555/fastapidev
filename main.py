4143e1548989523eb031450685060f6dc5b4333a
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-11-15 01:59:45 +0200

    Update request

--- main.py
+++ main.py
@@ -60,10 +60,23 @@ def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    
+
     index = finde_index_post(id)
     if index == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
     my_posts.pop(index)
     return Response(tatus_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = finde_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+
+    post_dict = post.model_dump(mode='json')
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"message": post_dict}
