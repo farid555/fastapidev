cefb297945e76749776028b7dbcfa9a722c87462
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2023-11-14 23:42:53 +0200

    Basic get && post

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import Body, FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your post"}
+
+
+@app.post("/createposts")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
